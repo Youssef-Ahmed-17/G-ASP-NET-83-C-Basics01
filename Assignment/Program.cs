934511f6503b25ac4c99f903b834beb5344e0209
Youssef <youssef.ahmed.2777@gmail.com>
2026-07-18 16:50:57 +0300

    Question 14

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -188,6 +188,25 @@ namespace Assignment
             //Console.WriteLine(title);
 
             #endregion
+
+
+            #region Question 14 
+            /*
+             Using title from the previous question, use ?? to print "Untitled" if title is null.
+            Then use ??= to assign title the value "Untitled" only if it's still null.
+             */
+
+            //Console.WriteLine(title ?? "Untitled");
+
+            //title ??= "Untitled";
+
+            //Console.WriteLine(title);
+
+            #endregion
+
+
+
+
         }
     }
 }
