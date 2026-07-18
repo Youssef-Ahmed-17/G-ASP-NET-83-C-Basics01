b7e1282a35505ccca2b13557addfcdee524c0a22
Youssef <youssef.ahmed.2777@gmail.com>
2026-07-18 15:57:39 +0300

    Question 01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        class Book
+        {
+            public string tittle;
+            public int pages;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Question 01 
+            //Create a Book object and store it in a variable of type object. Print it.
+            
+            Object book = new Book();
+            Console.WriteLine(book); 
+            
+            #endregion
+
+
         }
     }
 }
