76f0f0527a4b526bf5f58936c22760a4d4f3c2c3
Youssef <youssef.ahmed.2777@gmail.com>
2026-07-18 16:24:07 +0300

    Question 07

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -86,11 +86,21 @@ namespace Assignment
 
             #region Question 06 
             //Declare a double price = 49.99; then convert it into an int using a cast.
-            
+
             //double price = 49.99;
             //int intPrice = ( int ) price;
             //Console.WriteLine(intPrice);
 
+
+            #endregion
+
+            #region Question 07
+            // Given string pagesText = "464";, convert it into an int using the Convert class.
+
+
+            //string pagesText = "464";
+            //int convertedPages = Convert.ToInt32(pagesText);
+            //Console.WriteLine(convertedPages);
 
             #endregion
         }
