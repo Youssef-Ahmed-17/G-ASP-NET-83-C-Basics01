2ca42cab091726ae6c916c70a215b59f6466db9b
Youssef <youssef.ahmed.2777@gmail.com>
2026-07-18 16:31:28 +0300

    Question 09

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -123,7 +123,20 @@ namespace Assignment
 
             #endregion
 
+            #region Question 09 
+            /*Given int pages = 464;, convert it into a string using ToString() 
+            and print its type using GetType() to prove it's now a string.
+            */
 
-        }
+            //int pages = 464;
+            //string stringPages = pages.ToString();
+            //Console.WriteLine(stringPages.GetType()); 
+
+
+
+
+#endregion
+
+}
 }
 }
